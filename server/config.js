dd683ee2d9e93c3ee4728b4ea85defab01eb679d
Luke Lu <1349522569@qq.com>
2025-03-15 16:59:39 +0800

    修改中奖名单

--- server/config.js
+++ server/config.js
@@ -7,48 +7,99 @@
  * img: 图片地址
  */
 const prizes = [
-  {
-    type: 0,
-    count: 1000,
-    title: "",
-    text: "特别奖"
-  },
-  {
-    type: 1,
-    count: 2,
-    text: "特等奖",
-    title: "神秘大礼",
-    img: "../img/secrit.jpg"
-  },
-  {
-    type: 2,
-    count: 5,
-    text: "一等奖",
-    title: "Mac Pro",
-    img: "../img/mbp.jpg"
-  },
-  {
-    type: 3,
-    count: 6,
-    text: "二等奖",
-    title: "华为 Mate30",
-    img: "../img/huawei.png"
-  },
-  {
-    type: 4,
-    count: 7,
-    text: "三等奖",
-    title: "Ipad Mini5",
-    img: "../img/ipad.jpg"
-  },
-  {
-    type: 5,
-    count: 8,
-    text: "四等奖",
-    title: "大疆无人机",
-    img: "../img/spark.jpg"
-  },
-
+  [
+    {
+      "type": 11,
+      "count": 1,
+      "text": "王龙",
+      "title": "茅台生肖酒 一支",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 10,
+      "count": 2,
+      "text": "",
+      "title": "3D震动洁肤仪(2000HKD)",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 10,
+      "count": 2,
+      "text": "马宝君",
+      "title": "声波振动离子导入仪（价值2500HKD）",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 9,
+      "count": 1,
+      "text": "张乐烽",
+      "title": "荣耀平板电脑",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 8,
+      "count": 5,
+      "text": "陈日飙",
+      "title": "华为WATCH智能手表",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 7,
+      "count": 5,
+      "text": "",
+      "title": "巴寶莉女神香水礼盒",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 7,
+      "count": 30,
+      "text": "蔡惠青",
+      "title": "一家親酵素",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 6,
+      "count": 10,
+      "text": "唐建铭",
+      "title": "红葡萄酒",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 5,
+      "count": 10,
+      "text": "卢腾光",
+      "title": "白葡萄酒",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 4,
+      "count": 1000,
+      "text": "路  遥",
+      "title": "THC可持续研学所免费SCI论文咨询",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 3,
+      "count": 1,
+      "text": "黎  璇",
+      "title": "用友CRM系统 一年使用期",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 2,
+      "count": 50,
+      "text": "李  建",
+      "title": "茶叶",
+      "img": "../img/gift.png"
+    },
+    {
+      "type": 1,
+      "count": 60,
+      "text": "曾  许",
+      "title": "家里香 文昌鸡",
+      "img": "../img/gift.png"
+    }
+  ]
 ];
 
 /**
